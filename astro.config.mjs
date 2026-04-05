d44d30f032ac44392864b26fffd20530c2a10942
Raul r <prueba@correo.com>
2026-04-05 14:13:06 -0400

    cORRECCIONES dinámicos de todas las páginas para modo estático

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'static',
+  output: 'hybrid',
+  adapter: node({ mode: 'standalone' }),
   site: 'https://disenopaginas.cl',
   base: '/propiedades-demo',
   trailingSlash: 'ignore'
