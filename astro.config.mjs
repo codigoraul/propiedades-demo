6e399d37af484c5f6d39474bad7bc8e753a3d7cc
Raul r <prueba@correo.com>
2026-04-05 14:21:05 -0400

    Corre .js a v22 en workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'hybrid',
   adapter: node({ mode: 'standalone' }),
   site: 'https://disenopaginas.cl',
   base: '/propiedades-demo',
